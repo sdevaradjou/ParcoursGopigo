6a80e94fae8bd259220590e06707866e6481cb74
sdevaradjou <s.devaradjou@gmail.com>
2016-12-25 20:15:59 +0100

    premier commit (dessin de traits sur fond)

---
+++ app/src/main/java/com/paris8/pro/parcoursgopigo/MainActivity.java
@@ -0,0 +1,267 @@
+package com.paris8.pro.parcoursgopigo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+    MySurfaceView mySurfaceView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mySurfaceView = new MySurfaceView(this);
+        setContentView(mySurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mySurfaceView.onResumeMySurfaceView();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mySurfaceView.onPauseMySurfaceView();
+    }
+
+    class MySurfaceView extends SurfaceView implements Runnable {
+
+        Path path;
+
+        Thread thread = null;
+        SurfaceHolder surfaceHolder;
+        volatile boolean running = false;
+
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        Random r, aa, bb;
+
+        public MySurfaceView(Context context) {
+            super(context);
+            surfaceHolder = getHolder();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(10);
+            paint.setColor(Color.WHITE);
+            r = new Random();
+            aa = new Random();
+            bb = new Random();
+        }
+
+        /*@Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                path = new Path();
+                path.moveTo(event.getX(), event.getY());
+            } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                path.lineTo(event.getX(), event.getY());
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                path.lineTo(event.getX(), event.getY());
+            }
+
+            if (path != null) {
+                Canvas canvas = surfaceHolder.lockCanvas();
+                canvas.drawPath(path, paint);
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+
+            return true;
+        }*/
+
+        public void onResumeMySurfaceView() {
+            running = true;
+            thread = new Thread(this);
+            thread.start();
+        }
+
+        public void onPauseMySurfaceView() {
+            boolean retry = true;
+            running = false;
+            while (retry) {
+                try {
+                    thread.join();
+                    retry = false;
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            while (running) {
+                if (surfaceHolder.getSurface().isValid()) {
+                    Canvas canvas = surfaceHolder.lockCanvas();
+                    //... actual drawing on canvas
+
+                    int i1 = r.nextInt(4 - 1) + 1;
+                    int a = 0;
+                    int b = 0;
+                    path = new Path();
+                    path.moveTo(0, 0);
+
+                    for (int i = 0; i < 1000; i++) {
+                        path.lineTo(a,b++);
+                    }
+
+                    /*if (path != null) {
+                        canvas = surfaceHolder.lockCanvas();
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }*/
+
+                    canvas.drawPath(path, paint);
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/*
+import android.app.Activity;
+
+import android.content.Context;
+
+import android.graphics.Canvas;
+
+import android.graphics.Color;
+
+import android.graphics.Paint;
+
+import android.graphics.Path;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+
+
+public class MainActivity extends Activity {
+
+
+
+    */
+/**
+ * Called when the activity is first created.
+ *//*
+
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(new MyView(this));
+
+    }
+
+
+
+    public class MyView extends View {
+
+
+
+        class Pt{
+
+            float x, y;
+
+
+
+            Pt(float _x, float _y){
+
+                x = _x;
+
+                y = _y;
+
+            }
+
+        }
+
+
+
+        Pt[] myPath = { new Pt(100, 100),
+
+                new Pt(200, 200),
+
+                new Pt(200, 500),
+
+                new Pt(400, 500),
+
+                new Pt(400, 200)
+
+        };
+
+
+
+        public MyView(Context context) {
+
+            super(context);
+
+            // TODO Auto-generated constructor stub
+
+        }
+
+
+
+        @Override
+
+        protected void onDraw(Canvas canvas) {
+
+            // TODO Auto-generated method stub
+
+            super.onDraw(canvas);
+
+
+
+
+
+            Paint paint = new Paint();
+
+            paint.setColor(Color.WHITE);
+
+            paint.setStrokeWidth(3);
+
+            paint.setStyle(Paint.Style.STROKE);
+
+            Path path = new Path();
+
+
+
+            path.moveTo(myPath[0].x, myPath[0].y);
+
+            for (int i = 1; i < myPath.length; i++){
+
+                path.lineTo(myPath[i].x, myPath[i].y);
+
+            }
+
+            canvas.drawPath(path, paint);
+
+
+
+        }
+
+
+
+    }
+
+}*/
